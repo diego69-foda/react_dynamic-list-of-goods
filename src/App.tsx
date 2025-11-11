54fe67fd9be5cdcad715987dc2568d7e04ebe4ec
diego69-foda <diego.bkuhn@gmail.com>
2025-11-11 10:38:58 -0300

    Refactor App component for improved readability and consistency in API calls

--- src/App.tsx
+++ src/App.tsx
@@ -6,26 +6,39 @@ import { getAll, get5First, getRedGoods } from './api/goods';
 // import { getAll, get5First, getRed } from './api/goods';
 // or
 // import * as goodsAPI from './api/goods';
- 
+
 export const App: React.FC = () => {
   const [goods, setGoods] = React.useState<Good[]>([]);
+
   return (
     <div className="App">
-    <h1>Dynamic list of Goods</h1>
+      <h1>Dynamic list of Goods</h1>
 
-    <button type="button" data-cy="all-button" onClick={() => getAll()}>
-      Load all goods
-    </button>
+      <button
+        type="button"
+        data-cy="all-button"
+        onClick={() => getAll().then(setGoods)}
+      >
+        Load all goods
+      </button>
 
-    <button type="button" data-cy="first-five-button" onClick={() => get5First()}>
-      Load 5 first goods
-    </button>
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={() => get5First().then(setGoods)}
+      >
+        Load 5 first goods
+      </button>
 
-    <button type="button" data-cy="red-button" onClick={() => getRedGoods()}>
-      Load red goods
-    </button>
+      <button
+        type="button"
+        data-cy="red-button"
+        onClick={() => getRedGoods().then(setGoods)}
+      >
+        Load red goods
+      </button>
 
-    <GoodsList goods={[]} />
-  </div>
+      <GoodsList goods={goods} />
+    </div>
   );
-}
+};
