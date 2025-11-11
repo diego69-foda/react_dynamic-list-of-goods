514e00d81dd2783c6ae6682a2c81e9279e747c9c
diego69-foda <diego.bkuhn@gmail.com>
2025-11-11 10:38:38 -0300

    Refactor goods retrieval functions for consistent sorting and improve App component structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,31 @@
 import React from 'react';
 import './App.scss';
+import { Good } from './types/Good';
 import { GoodsList } from './GoodsList';
 import { getAll, get5First, getRedGoods } from './api/goods';
 // import { getAll, get5First, getRed } from './api/goods';
 // or
 // import * as goodsAPI from './api/goods';
-
-export const App: React.FC = () => (
-  <div className="App">
+ 
+export const App: React.FC = () => {
+  const [goods, setGoods] = React.useState<Good[]>([]);
+  return (
+    <div className="App">
     <h1>Dynamic list of Goods</h1>
 
-    <button type="button" data-cy="all-button" onClick={getAll()}>
+    <button type="button" data-cy="all-button" onClick={() => getAll()}>
       Load all goods
     </button>
 
-    <button type="button" data-cy="first-five-button" onClick={get5First()}>
+    <button type="button" data-cy="first-five-button" onClick={() => get5First()}>
       Load 5 first goods
     </button>
 
-    <button type="button" data-cy="red-button" onClick={getRedGoods()}>
+    <button type="button" data-cy="red-button" onClick={() => getRedGoods()}>
       Load red goods
     </button>
 
     <GoodsList goods={[]} />
   </div>
-);
+  );
+}
